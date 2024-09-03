2819109f855954d742e6af2a655bc4934b6c5c7e
fath-ens <fatihen.es99@outlook.com>
2024-09-03 11:18:21 +0300

    initial commit

---
+++ src/main/java/com/fathens/pricerate/PriceRateApplication.java
@@ -0,0 +1,13 @@
+package com.fathens.pricerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceRateApplication.class, args);
+    }
+
+}
